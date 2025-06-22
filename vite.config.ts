98e37cdd90a79ed328372fd0ecff842773f6ccfa
Jmsandi2k25 <jmsandi2k25@gmail.com>
2025-06-22 12:04:33 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,22 @@ import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import * as dotenv from 'dotenv';
 import { execSync } from 'child_process';
+import { vitePlugin as remix } from '@remix-run/dev';
+import { installGlobals } from '@remix-run/node';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { vercelPreset } from '@vercel/remix/vite';
+ 
+installGlobals();
+ 
+export default defineConfig({
+  plugins: [
+    remix({
+      presets: [vercelPreset()],
+    }),
+    tsconfigPaths(),
+  ],
+});
 
 dotenv.config();
 
